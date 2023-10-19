29e782b9904469725058e3d98b29d090822f4c0a
Diego Higuera <dhiguera09@gmail.com>
2023-10-18 22:25:20 -0500

    Conectando la aplicación con Base de Datos SQL

--- client/main.js
+++ client/main.js
@@ -1,14 +1,34 @@
 import { io } from "https://cdn.socket.io/4.3.2/socket.io.esm.min.js";
 
-const socket = io();
+const getUsername = async () => {
+  const username = localStorage.getItem("username")
+  if (username) {
+    console.log(`User existed ${username}`);
+  }
+
+  const res = await fetch("https://random-data-api.com/api/users/random_user");
+  const { username: randomUsername } = await res.json();
+
+  localStorage.setItem('username', randomUsername)
+  return randomUsername
+}
+
+const socket = io({
+  auth: {  
+    username: await getUsername(),
+    serverOffset: 0
+  }
+});
 
 const messages = document.getElementById("messages");
 const form = document.getElementById("form");
 const input = document.getElementById("input");
 
-socket.on("chat message", (msg) => {
-  const item = `<li>${msg}</li>`;
+
+socket.on("chat message", (msg, serverOffset) => {
+  const item = `<li><p>${msg}</p><small>${username}</small></li>`;
   messages.insertAdjacentHTML("beforeend", item);
+  socket.auth.serverOffset = serverOffset
 });
 
 form.addEventListener("submit", (e) => {
--- server/index.js
+++ server/index.js
@@ -1,5 +1,12 @@
 import express from "express";
 import logger from "morgan";
+import dotenv from "dotenv";
+
+if (process.env.NODE_ENV !== "production") {
+  dotenv.config();
+}
+
+import { createClient } from "@libsql/client";
 
 import { Server } from "socket.io";
 import { createServer } from "node:http";
@@ -9,24 +16,65 @@ const port = process.env.PORT ?? 3000;
 const app = express();
 const server = createServer(app);
 const io = new Server(server, {
-    connectionStateRecovery: {
-        
-    }
+  connectionStateRecovery: {},
 });
+
+const db = createClient({
+  url: "libsql://willing-mighty-diegohs07.turso.io",
+  authToken: process.env.BD_TOKEN,
+});
+
+await db.execute(`
+  CREATE TABLE IF NOT EXISTS messages (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    content TEXT,
+    user TEXT
+  )
+`);
 
 app.use(logger("dev"));
 app.use(express.static(process.cwd() + "/client"));
 
-io.on("connection", (socket) => {
+io.on("connection", async (socket) => {
   console.log("A user has connected!!");
 
   socket.on("disconnect", () => {
     console.log("an user has disconnected");
   });
 
-  socket.on("chat message", (msg) => {
-    io.emit("chat message", msg);
+  socket.on("chat message", async (msg) => {
+    let result;
+    const username = socket.handshake.auth.username ?? 'anonymous'
+    try {
+      result = await db.execute({
+        sql: `INSERT INTO messages (content, user) VALUES (:msg, :username)`,
+        args: { msg, username },
+      });
+    } catch (e) {
+      console.error(e)
+      return
+    }
+
+    io.emit('chat message', msg, result.lastInsertRowid.toString(), username)
   });
+
+  if (!socket.recovered) {
+    try {
+      const results = await db.execute({
+        sql: 'SELECT id, content, user FROM messages WHERE id > ?',
+        args: [socket.handshake.auth.serverOffset ?? 0]
+      })
+
+      results.rows.forEach(row => {
+        socket.emit('chat message', row.content, row.id.toString(), row.username)
+      })
+
+    } catch (e) {
+      console.error(e.message)
+      return
+    }
+  }
+
 });
 
 app.get("/", (req, res) => {
